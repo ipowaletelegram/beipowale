4e1e61dd316cbba2c9c3e44d6b7434c9ce25ad7a
ipowaletelegram <ipowalee@gmail.com>
2026-07-14 00:11:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder=".")
 
 @app.route("/")
 def home():
-    return send_from_directory(".", "login.html")
+    return send_from_directory(".", "index.html")
 
 @app.route("/<path:path>")
 def static_files(path):
