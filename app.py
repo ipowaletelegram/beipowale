a8ea51ef0a1ca2d46fabc5e3a07b41a396f57df4
ipowaletelegram <ipowalee@gmail.com>
2026-07-13 23:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=".")
 
 @app.route("/")
 def home():
     return send_from_directory(".", "index.html")
 
-@app.route("/<path:filename>")
-def files(filename):
-    return send_from_directory(".", filename)
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory(".", path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
