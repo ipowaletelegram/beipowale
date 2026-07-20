7d2fd92f33acce966922230e97b647700c6b4a0e
ipowaletelegram <ipowalee@gmail.com>
2026-07-20 15:19:25 +0530

    Update allotment.py

--- routes/allotment.py
+++ routes/allotment.py
@@ -1,1 +1,11 @@
+from flask import Blueprint, render_template
 
+allotment_bp = Blueprint(
+    "allotment",
+    __name__
+)
+
+@allotment_bp.route("/allotment")
+def allotment():
+
+    return render_template("allotment.html")
