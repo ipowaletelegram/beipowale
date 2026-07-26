38cdb5b0a8b6596cc19b1067305aa8335570c826
ipowaletelegram <ipowalee@gmail.com>
2026-07-26 16:45:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-import time
+from datetime import datetime
+import pytz
 app = Flask(__name__)
 
 @app.route("/")
@@ -24,9 +25,18 @@ def terms():
 
 @app.route("/ipo-calendar")
 def ipo_calendar():
+
+    india = pytz.timezone("Asia/Kolkata")
+    now = datetime.now(india)
+
+    # Show calendar only after 6 AM
+    show_calendar = now.hour >= 6
+
     return render_template(
         "ipo-calendar.html",
-        timestamp=int(time.time())
+        show_calendar=show_calendar,
+        current_time=now.strftime("%d %B %Y %I:%M %p"),
+        timestamp=int(now.timestamp())
     )
 
 if __name__ == "__main__":
